3946eb36cd5041cc1c66b5daf6b07e9cae512082
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-05-21 16:13:56 +0500

    savollar massivga olindi

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,8 +15,42 @@ class TestController extends Controller
         $k=0;
         $savollar=[];
        $n=strlen($text);
-       $mas=explode('?',$text);
-dd($mas);
+       $mas=explode("ANSWER: A",$text);
+
+
+        $n=count($mas);
+        echo $n;
+        for($j=0;$j<$n-1;$j++){
+            $m=strlen($mas[$j]);
+
+
+                    $savol=[];
+                    $savol[0]="";
+                    $savol[1]="";
+                    $savol[2]="";
+                    $savol[3]="";
+                    $savol[4]="";
+                    $z=0;
+                    for($k=0;$k<$m;$k++){
+
+                        if(($mas[$j][$k]=='A' && $mas[$j][$k+1]=='.' && $mas[$j][$k+2]==' ')
+                            || ($mas[$j][$k]=='B' && $mas[$j][$k+1]=='.' && $mas[$j][$k+2]==' ')
+                            || ($mas[$j][$k]=='C' && $mas[$j][$k+1]=='.' && $mas[$j][$k+2]==' ')
+                            ||($mas[$j][$k]=='D' && $mas[$j][$k+1]=='.' && $mas[$j][$k+2]==' ') ){
+                            $k+=2; $z+=1;
+
+                        }
+                        $savol[$z].=$mas[$j][$k];
+
+                    }
+                    $savollar[]=$savol;
+
+
+
+
+            }
+
+        dd($savollar);
 
     }
 }
